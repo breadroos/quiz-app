d10318000e92679bf40de3a91cd7c1a9e72b0e87
Brett Roos <brettroos60@gmail.com>
2019-10-23 14:11:00 -0700

    started merging in question info from STORE

--- index.js
+++ index.js
@@ -6,62 +6,62 @@ const STORE = {
     // QUESTION: 1
     {
       //question will be a single string with the question for the Quiz
-  question: "What type of whale is the biggest animal in the world?",
-    // options is going to have an array of data that has the multiple choice options for the question.
-  options: [
-    "Killer Whale",
-    "Blue Whale",
-    "Sperm Whale",
-    "Humpback Whale"
-  ],
-  // answer will be a single string with the answer that we can compare the option selected to
-  answer: "Blue Whale"
+      question: "What type of whale is the biggest animal in the world?",
+      // options is going to have an array of data that has the multiple choice options for the question.
+      options: [
+        "Killer Whale",
+        "Blue Whale",
+        "Sperm Whale",
+        "Humpback Whale"
+      ],
+      // answer will be a single string with the answer that we can compare the option selected to
+      answer: "Blue Whale"
     },
-// QUESTION: 2
-  {
-    question: "Who was nicknamed “Trey” (Three in card terminology), when he was young?",
-    options: [
-      "Bill Gates",
-      "Donald Trump",
-      "Mark Zuckerberg",
-      "Kevin Bacon",
-    ],
-    answer: "Bill Gates"
-  },
-  // QUESTION: 3
-  {
-    question: "His tomb in France reads “He taught us how to become free” as his writings were instrumental in creating a spirit of freedom during the French Revolution. Who was he?",
-    options: [
-      "NAPOLEON BONAPARTE",
-      "LOUIS ANTOINE DE SAINT-JUST",
-      "VOLTAIRE",
-      "EMMANUEL JOSEPH SIEYÈS",
-    ],
-    answer: "VOLTAIRE"
-  },
-  // QUESTION: 4
-  {
-    question: "Which first electrical item did Thomas Edison invent?",
-    options: [
-      "Cell Phone",
-      "Analog Clocks",
-      "Lightbulb",
-      "Dishwasher"
-    ],
-    answer: "Lightbulb"
-  },
-  {
-    // QUESTION: 5
-    question: "Who hunts most – male or female lion?",
-    options: [
-      "Male",
-      "Female",
-      "Both hunt an equal amount",
-      "It varies depending on the season"
-    ],
-    answer: "Female"
-  }
-],
+    // QUESTION: 2
+    {
+      question: "Who was nicknamed “Trey” (Three in card terminology), when he was young?",
+      options: [
+        "Bill Gates",
+        "Donald Trump",
+        "Mark Zuckerberg",
+        "Kevin Bacon",
+      ],
+      answer: "Bill Gates"
+    },
+    // QUESTION: 3
+    {
+      question: "His tomb in France reads “He taught us how to become free” as his writings were instrumental in creating a spirit of freedom during the French Revolution. Who was he?",
+      options: [
+        "NAPOLEON BONAPARTE",
+        "LOUIS ANTOINE DE SAINT-JUST",
+        "VOLTAIRE",
+        "EMMANUEL JOSEPH SIEYÈS",
+      ],
+      answer: "VOLTAIRE"
+    },
+    // QUESTION: 4
+    {
+      question: "Which first electrical item did Thomas Edison invent?",
+      options: [
+        "Cell Phone",
+        "Analog Clocks",
+        "Lightbulb",
+        "Dishwasher"
+      ],
+      answer: "Lightbulb"
+    },
+    {
+      // QUESTION: 5
+      question: "Who hunts most – male or female lion?",
+      options: [
+        "Male",
+        "Female",
+        "Both hunt an equal amount",
+        "It varies depending on the season"
+      ],
+      answer: "Female"
+    }
+  ],
 };
 
 // --------------------------------------------------------------------------------
@@ -72,16 +72,19 @@ $(function startQuiz() {
   $('.start-quiz-button').on('click', function(event) {
     console.log('The start button is working');
     renderQuestion();
-  }
-);
-})
+  });
+});
 //
 // function currentQuestionAndScore(){
 //}
 
+var question = STORE.questions[0].question;
+var option1 = STORE.questions[0].options[0];
 
-function renderQuestion(){
+function renderQuestion() {
   console.log('the start button is telling me to render a question.');
+  console.log(question);
+  console.log(option1);
   $('.description-container').replaceWith(`<div class="question-container">
     <div class="quiz-title">
       General Trivia Quiz!
@@ -97,7 +100,7 @@ function renderQuestion(){
     </div>
 
     <div class="question-box-title">
-      What is the best way to have an example question box?
+      ${STORE.questions[0].question}
     </div>
     <!-- specifics of what the basic trivia pertains to -->
       <div class="question-box-content">
@@ -105,7 +108,7 @@ function renderQuestion(){
           <div class="question-radio-options">
             <div class="option-1">
               <div class="option-1">
-              <input type="radio" name="option" value="You can do it this way." id="option-1">
+              <input type="radio" name="option" value="${STORE.questions[0].options[0]}" id="option-1">
               <label for="option-1">You can do it this way.</label>
             </div>
             <div class="option-2">
@@ -126,8 +129,9 @@ function renderQuestion(){
       </div>
     </div>
   </div>
-`)
+`);
   //need to find a question from the store.js file, and then render the html on the page for the user.
+};
 
 $(function nextQuestion() {
   //this moves them on to the next question
@@ -178,43 +182,10 @@ $(function nextQuestion() {
         </div>
       </div>
     </div>
-`)
-  })
-})
-// start with a hardcoded version to figure out the jquery needed to switch our the content on the page
+    `);
+  });
+});
 
-//   let questionHTML = $(`
-//       <div class="question-box-title">
-//         LOAD IN A QUESTION FROM STORE.JS HERE.
-//       </div>
-//       <!-- specifics of what the basic trivia pertains to -->
-//         <div class="question-box-content">
-//           <form action="" class="question-options">
-//             <div class="question-radio-options">
-//               <div class="option-1">
-//                 <div class="option-1">
-//                 <input type="radio" name="option" value="You can do it this way." id="option-1">
-//                 <label for="option-1">LOAD IN A QUESTION OPTION FROM STORE.JS HERE</label>
-//               </div>
-//               <div class="option-2">
-//                 <input type="radio" name="option" value="You can do it this way as well." id="option-2">
-//                 <label for="option-2">LOAD IN A QUESTION OPTION FROM STORE.JS HERE</label>
-//               </div>
-//               <div class="option-3">
-//                 <input type="radio" name="option" value="You could even do it this way if you wanted." id="option-3">
-//                 <label for="option-3">LOAD IN A QUESTION OPTION FROM STORE.JS HERE</label>
-//               </div>
-//               <div class="option-4">
-//                 <input type="radio" name="option" value="Even better yet, this way." id="option-4">
-//                 <label for="option-4">LOAD IN A QUESTION OPTION FROM STORE.JS HERE</label>
-//               </div>
-//             </div>
-//               <input type="submit" name="submit-button" class="question-submit-button" value="Submit">
-//           </form>
-//         </div>
-//       </div>
-//     `)
-};
 
 
 // function scoreTracker(){
